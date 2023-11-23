4c0c36f69de02f7b0b0252e0bf1cd71ca96ecd13
Heyward Fann <fannheyward@gmail.com>
2023-11-23 19:51:38 +0800

    chore: minify

--- esbuild.js
+++ esbuild.js
@@ -3,7 +3,7 @@ async function start() {
   await require('esbuild').build({
     entryPoints: ['src/index.ts'],
     bundle: true,
-    minify: process.env.NODE_ENV === 'production',
+    minify: true,
     sourcemap: process.env.NODE_ENV === 'development',
     mainFields: ['module', 'main'],
     external: ['coc.nvim'],
